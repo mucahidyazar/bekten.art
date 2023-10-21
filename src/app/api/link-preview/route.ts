11af374ca6c47781898033fda35770d44e4d3b05
Mucahid Yazar <mucahidyazar@gmail.com>
2023-10-21 09:55:14 +0300

    fix: build errors

--- src/app/api/link-preview/route.ts
+++ src/app/api/link-preview/route.ts
@@ -2,12 +2,10 @@ import { load } from 'cheerio'
 
 export async function POST(request: Request) {
   try {
-    console.log('x1')
     const res = await request.json()
 
     const links: string[] = res.links
 
-    console.log('x2')
     const promises = links.map(async (link) => {
       const response = await fetch(link)
       const data = await response.text()
@@ -24,10 +22,8 @@ export async function POST(request: Request) {
 
       return { title, description, image, url }
     })
-    console.log('x3')
 
     const results = await Promise.all(promises)
-    console.log('x4')
     return (Response as any).json({ data: results })
   } catch (error) {
     console.error(`Error while fetching link preview data: ${error}`)
