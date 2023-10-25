75e4438083133087e08000e120e15e4fd758aab3
Mucahid Yazar <mucahidyazar@gmail.com>
2023-10-25 09:57:11 +0300

    feat: add auth management by google and github

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,6 +8,7 @@ import 'swiper/css/effect-fade'
 
 import {Lora} from 'next/font/google'
 import {notFound} from 'next/navigation'
+import {getServerSession} from 'next-auth'
 import {NextIntlClientProvider} from 'next-intl'
 
 import {Footer} from '@/components/Footer'
@@ -15,6 +16,8 @@ import {AppTools} from '@/components/molecules/AppTools'
 import {Sidebar} from '@/components/organisms/Sidebar'
 import {ThemeProvider} from '@/components/providers/ThemeProvider'
 import {prepareMetadata} from '@/utils/prepareMetadata'
+
+import {SessionProvider} from './SessionProvider'
 
 const lora = Lora({subsets: ['latin']})
 
@@ -41,6 +44,7 @@ export default async function RootLayout({
   children,
   params: {locale},
 }: LayoutProps) {
+  const session = await getServerSession()
   const messages = await getMessages(locale)
 
   return (
@@ -49,25 +53,28 @@ export default async function RootLayout({
       <body
         className={`${lora.className} flex flex-col overflow-x-hidden bg-background`}
       >
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            themes={['dark', 'light', 'navy']}
-          >
-            <div className="mx-auto flex h-full w-full max-w-full flex-grow flex-col gap-5 px-0 lg:max-w-7xl lg:flex-row lg:px-4">
-              <Sidebar />
-              <div className="relative h-full px-4 lg:ml-auto lg:w-[calc(100%-15rem)] lg:min-w-[calc(100%-15rem)] lg:px-0 lg:pb-8 lg:pt-10">
-                <div className="pointer-events-none sticky left-0 top-0 z-10 -mt-10 h-10 w-full bg-gradient-to-b from-background to-transparent lg:mb-10" />
-                {children}
-              </div>
-              <Footer className="flex flex-col items-center gap-4 py-8 lg:hidden" />
+        <SessionProvider session={session}>
+          <NextIntlClientProvider locale={locale} messages={messages}>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              themes={['dark', 'light', 'navy']}
+            >
+              <div className="mx-auto flex h-full w-full max-w-full flex-grow flex-col gap-5 px-0 lg:max-w-7xl lg:flex-row lg:px-4">
+                <Sidebar />
+                <div className="relative h-full px-4 lg:ml-auto lg:w-[calc(100%-15rem)] lg:min-w-[calc(100%-15rem)] lg:px-0 lg:pb-8 lg:pt-10">
+                  <div className="pointer-events-none sticky left-0 top-0 z-10 -mt-10 h-10 w-full bg-gradient-to-b from-background to-transparent lg:mb-10" />
+                  {children}
+                </div>
+                <Footer className="flex flex-col items-center gap-4 py-8 lg:hidden" />
+                <Footer className="flex flex-col items-center gap-4 py-8 lg:hidden" />
 
-              <AppTools />
-            </div>
-          </ThemeProvider>
-        </NextIntlClientProvider>
+                <AppTools />
+              </div>
+            </ThemeProvider>
+          </NextIntlClientProvider>
+        </SessionProvider>
       </body>
     </html>
   )
