937580d0d4d4db1baf9fc6b9e8b61d6c5fb70fc0
Mucahid Yazar <mucahidyazar@gmail.com>
2023-11-01 20:59:17 +0300

    feat: next.js for saas

---
+++ src/app/[locale]/(root)/layout.tsx
@@ -0,0 +1,36 @@
+import 'swiper/css'
+import 'swiper/css/autoplay'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+import 'swiper/css/effect-fade'
+
+import {Footer} from '@/components/Footer'
+import {AppTools} from '@/components/molecules/AppTools'
+import {Sidebar} from '@/components/organisms/Sidebar'
+import {prepareMetadata} from '@/utils/prepareMetadata'
+
+export function generateMetadata() {
+  return prepareMetadata()
+}
+
+type LayoutProps = {
+  children: React.ReactNode
+  params: {
+    locale: string
+  }
+}
+export default async function RootLayout({children}: LayoutProps) {
+  return (
+    <div className="mx-auto flex h-full w-full max-w-full flex-grow flex-col gap-5 px-0 lg:max-w-7xl lg:flex-row lg:px-4">
+      <Sidebar />
+      <div className="relative h-full px-4 lg:ml-auto lg:w-[calc(100%-15rem)] lg:min-w-[calc(100%-15rem)] lg:px-0 lg:pb-8 lg:pt-10">
+        <div className="pointer-events-none sticky left-0 top-0 z-10 -mt-10 h-10 w-full bg-gradient-to-b from-background to-transparent lg:mb-10" />
+        {children}
+      </div>
+      <Footer className="flex flex-col items-center gap-4 py-8 lg:hidden" />
+
+      <AppTools />
+    </div>
+  )
+}
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,18 +1,9 @@
 import './global.css'
-import 'swiper/css'
-import 'swiper/css/autoplay'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
-import 'swiper/css/scrollbar'
-import 'swiper/css/effect-fade'
 
 import {Lora} from 'next/font/google'
 import {notFound} from 'next/navigation'
 import {NextIntlClientProvider} from 'next-intl'
 
-import {Footer} from '@/components/Footer'
-import {AppTools} from '@/components/molecules/AppTools'
-import {Sidebar} from '@/components/organisms/Sidebar'
 import {ThemeProvider} from '@/components/providers/ThemeProvider'
 import {prepareMetadata} from '@/utils/prepareMetadata'
 
@@ -56,16 +47,7 @@ export default async function RootLayout({
             enableSystem
             themes={['dark', 'light', 'navy']}
           >
-            <div className="mx-auto flex h-full w-full max-w-full flex-grow flex-col gap-5 px-0 lg:max-w-7xl lg:flex-row lg:px-4">
-              <Sidebar />
-              <div className="relative h-full px-4 lg:ml-auto lg:w-[calc(100%-15rem)] lg:min-w-[calc(100%-15rem)] lg:px-0 lg:pb-8 lg:pt-10">
-                <div className="pointer-events-none sticky left-0 top-0 z-10 -mt-10 h-10 w-full bg-gradient-to-b from-background to-transparent lg:mb-10" />
-                {children}
-              </div>
-              <Footer className="flex flex-col items-center gap-4 py-8 lg:hidden" />
-
-              <AppTools />
-            </div>
+            {children}
           </ThemeProvider>
         </NextIntlClientProvider>
       </body>
