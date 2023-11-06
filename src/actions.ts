6545582f567b273661d3debd6daac10b2bb37e46
Mucahid Yazar <mucahidyazar@gmail.com>
2023-11-07 01:12:45 +0300

    Add user permissions and update artwork creation

--- src/actions.ts
+++ src/actions.ts
@@ -18,6 +18,22 @@ export async function createArtwork(artwork: any) {
   redirect('/store');
 }
 
+export async function updatePermission(id: string, updates: any) {
+  try {
+    // updates has also socials value of Social model inside the user
+    // update them also too
+    const data = JSON.parse(JSON.stringify(updates))
+    await db.user.update({
+      data, where: { id }
+    })
+    revalidatePath(`/profile/${id}`);
+  } catch (error) {
+    console.log(error)
+    return error
+  }
+  redirect(`/profile/${id}`);
+}
+
 export async function updateUser(id: string, updates: any) {
   try {
     // updates has also socials value of Social model inside the user
