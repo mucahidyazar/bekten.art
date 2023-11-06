8a45bd4a730883baf2e55b885d3e338c272ca36f
Mucahid Yazar <mucahidyazar@gmail.com>
2023-11-06 23:10:26 +0300

    Add updateUser function to update user data including socials value

--- src/actions.ts
+++ src/actions.ts
@@ -17,3 +17,25 @@ export async function createArtwork(artwork: any) {
   }
   redirect('/store');
 }
+
+export async function updateUser(id: string, updates: any) {
+  try {
+    // updates has also socials value of Social model inside the user
+    // update them also too
+    const data = JSON.parse(JSON.stringify(updates))
+    await db.user.update({
+      data: {
+        ...data,
+        socials: {
+          deleteMany: {},
+          create: data.socials
+        }
+      }, where: { id }
+    })
+    revalidatePath(`/profile/${id}`);
+  } catch (error) {
+    console.log(error)
+    return error
+  }
+  redirect(`/profile/${id}`);
+}
