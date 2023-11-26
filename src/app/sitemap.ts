1cbbc87a00109d76a1f8c7c157651b0851f45e4c
Mucahid Yazar <mucahidyazar@gmail.com>
2023-11-26 20:00:38 +0300

    fix: add new URLs to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -15,16 +15,40 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: 'https://bekten.art/gallery',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://bekten.art/news',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
       url: 'https://bekten.art/contact',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.5,
     },
     {
-      url: 'https://bekten.art/press',
+      url: 'https://bekten.art/store',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.5,
+    },
+    {
+      url: 'https://bekten.art/sign-in',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.1,
+    },
+    {
+      url: 'https://bekten.art/sign-up',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.1,
     },
   ]
 }
